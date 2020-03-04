da6fad12a082c6d34ec731b73b396213aae9b651
Filip Cwiek <fifek.inex@gmail.com>
2020-03-04 19:53:23 +0100

    Extended if else if window with results

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ if (randomNumber == 1) {
     computerMove = 'nozyce';
 }
 
-printMessage('Mój ruch to: ' + computerMove);
+printMessage('Komputera ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -28,5 +28,23 @@ if (playerInput == '1') {
     playerMove = 'nozyce';
 }
 
-
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nozyce') {
+    printMessage('Komputer wygrał!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+    printMessage('Remis!');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis');
+} else if (computerMove == 'papier' && playerMove == 'nozyce') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Komputer wygrał!');
+} else if (computerMove == 'nozyce' && playerMove == 'nozyce') {
+    printMessage('Remis');
+} else if (computerMove == 'nozyce' && playerMove == 'papier') {
+    printMessage('Komputer wygrał!');
+} else if (computerMove == 'nozyce' && playerMove == 'kamień')
+    printMessage('Ty wygrywasz!');

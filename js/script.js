5fb6ea9ad89f32dd8d05ac42e45ca5f4e3258f5b
Filip Cwiek <fifek.inex@gmail.com>
2020-03-10 18:27:18 +0100

    Delete empty spaces

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ let draws = 0;
 
 function playGame(playerInput) {
     clearMessages()
-        // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
     function getMoveName(argMoveId) {
         if (argMoveId == 1) {
@@ -18,9 +17,7 @@ function playGame(playerInput) {
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
         return 'nieznany ruch';
     }
-
     //dodalem jesli ktos wygra to do zmiennej playerWins lub computerWins inkrementuje i dodaje plus 1
-
     function displayResults(argComputerMove, argPlayerMove) {
         if (argComputerMove == 'kamień' && argPlayerMove == 'papier') {
             printMessage('Ty wygrywasz!');
@@ -50,49 +47,10 @@ function playGame(playerInput) {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     console.log('Wylosowana liczba to: ' + randomNumber);
     let argComputerMove = getMoveName(randomNumber);
-
-    // if (randomNumber == 1) {
-    //     computerMove = 'kamień';
-    // } else if (randomNumber == '2') {
-    //     computerMove = 'papier';
-    // } else if (randomNumber == '3') {
-    //     computerMove = 'nozyce';
-    // }
-
     printMessage('Komputera ruch to: ' + argComputerMove);
-
-    // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
     console.log('Gracz wpisał: ' + playerInput);
     let argPlayerMove = getMoveName(playerInput);
-
-    // if (playerInput == '1') {
-    //     playerMove = 'kamień';
-    // } else if (playerInput == '2') {
-    //     playerMove = 'papier';
-    // } else if (playerInput == '3') {
-    //     playerMove = 'nozyce';
-    // }
-
     printMessage('Twój ruch to: ' + argPlayerMove);
-
-    // if (computerMove == 'kamień' && playerMove == 'papier') {
-    //     printMessage('Ty wygrywasz!');
-    // } else if (computerMove == 'kamień' && playerMove == 'nozyce') {
-    //     printMessage('Komputer wygrał!');
-    // } else if (computerMove == 'papier' && playerMove == 'nozyce') {
-    //     printMessage('Ty wygrywasz!');
-    // } else if (computerMove == 'papier' && playerMove == 'kamień') {
-    //     printMessage('Komputer wygrał!');
-    // } else if (computerMove == 'nozyce' && playerMove == 'papier') {
-    //     printMessage('Komputer wygrał!');
-    // } else if (computerMove == 'nozyce' && playerMove == 'kamień') {
-    //     printMessage('Ty wygrywasz!');
-    // } else if (playerMove == 'nieznany ruch') {
-    //     printMessage('Przegrana walkowerem')
-    // } else(computerMove == playerMove)
-    // printMessage('Remis')
-
-
 
     displayResults(argComputerMove, argPlayerMove, playerWins, computerWins)
         //szukam po id w DOM i pokazuje w niej playerwins/computer wins
@@ -101,8 +59,8 @@ function playGame(playerInput) {
     //tu do displayresults dodalem playerWins i computerWins
 }
 
+//dodatkowe zadanie
 let messagesAnimation = document.getElementById('messages');
-
 document.getElementById('play-rock').addEventListener('click', function() {
     playGame(1);
     this.classList.toggle('active');
